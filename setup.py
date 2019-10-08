494f7d69c06091c9de28e2636c54f04ff18f789b
Aljosha Friemann <aljosha.friemann@clark.de>
2019-10-08 18:44:18 +0200

    documentation and package config

--- setup.py
+++ setup.py
@@ -1,48 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright © 2018 Clark Germany GmbH
-"""
-.. module:: TODO
-   :platform: Unix
-   :synopsis: TODO.
 
-.. moduleauthor:: Aljosha Friemann aljosha.friemann@clark.de
+from setuptools import setup
 
-"""
-
-import os
-import sys
-from subprocess import PIPE, Popen
-
-from kinja import __version__
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
-
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname), 'r').read()
-
-
-setup(name="kinja",
-      author="Aljosha Friemann",
-      author_email="aljosha.friemann@clark.de",
-      description="Script to template files",
-      url="https://www.github.com/ClarkSource/kinja.git",
-      download_url="https://github.com/ClarkSource/kinja/archive/{}.tar.gz".format(
-          __version__),
-      keywords=[],
-      version=__version__,
-      # license          = read('LICENSE.txt'),
-      # long_description = read('README.rst'),
-      install_requires=['click', 'Jinja2', 'PyYAML'],
-      extras_require={':python_version == "2.7"': ['futures']},
-      entry_points={'console_scripts': ['kinja=kinja.cli:root']},
-      classifiers=[],
-      packages=find_packages(),
-      platforms='linux'
-      )
-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
+setup(
+    entry_points={'console_scripts': ['kinja=kinja.cli:root']}
+)
